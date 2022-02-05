9fd7f27677bb3143c419a7ff5ffc042040f7376b
Zrzzzz <1160026659@qq.com>
2022-02-05 15:29:21 +0800

    [Fix] - 修改文件读取逻辑

--- api/router.go
+++ api/router.go
@@ -1,20 +1,11 @@
 package api
 
 import (
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/tls"
-	"crypto/x509"
-	"crypto/x509/pkix"
-	"encoding/pem"
-	"math/big"
-	"net/http"
 	"qnhd/api/v1/backend"
 	"qnhd/api/v1/frontend"
 	"qnhd/middleware/crossfield"
 	"qnhd/pkg/setting"
 	"qnhd/pkg/upload"
-	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -30,7 +21,7 @@ func InitRouter() (r *gin.Engine) {
 	// r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	// 解决跨域问题
 	r.Use(crossfield.CrossField())
-	r.StaticFS("/upload/images", http.Dir(upload.GetImageFullPath()))
+	r.StaticFS("/upload/images", gin.Dir(upload.GetImageFullPath(), false))
 
 	avb := r.Group("/api/v1/b")
 	backend.Setup(avb)
@@ -38,38 +29,4 @@ func InitRouter() (r *gin.Engine) {
 	frontend.Setup(avf)
 
 	return r
-}
-
-func InitTlsConfig() *tls.Config {
-	cert := &x509.Certificate{
-		SerialNumber: big.NewInt(1658),
-		Subject: pkix.Name{
-			Organization:  []string{"ORGANIZATION_NAME"},
-			Country:       []string{"COUNTRY_CODE"},
-			Province:      []string{"PROVINCE"},
-			Locality:      []string{"CITY"},
-			StreetAddress: []string{"ADDRESS"},
-			PostalCode:    []string{"POSTAL_CODE"},
-		},
-		NotBefore:    time.Now(),
-		NotAfter:     time.Now().AddDate(10, 0, 0),
-		SubjectKeyId: []byte{1, 2, 3, 4, 6},
-		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
-		KeyUsage:     x509.KeyUsageDigitalSignature,
-	}
-	priv, _ := rsa.GenerateKey(rand.Reader, 2048)
-	pub := &priv.PublicKey
-
-	// Sign the certificate
-	certificate, _ := x509.CreateCertificate(rand.Reader, cert, cert, pub, priv)
-
-	certBytes := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certificate})
-	keyBytes := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(priv)})
-
-	// Generate a key pair from your pem-encoded cert and key ([]byte).
-	x509Cert, _ := tls.X509KeyPair(certBytes, keyBytes)
-
-	tlsConfig := &tls.Config{
-		Certificates: []tls.Certificate{x509Cert}}
-	return tlsConfig
 }
