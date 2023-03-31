cf24e8b2f95d9fe64e312ea55fa59feccd9208ea
Zrzzzz <1160026659@qq.com>
2023-03-31 15:17:24 +0800

    [Fix] - 修复缩进

--- api/v1/backend/avatar_frame.go
+++ api/v1/backend/avatar_frame.go
@@ -22,7 +22,7 @@ func GetAllAvatarFrame(c *gin.Context) {
 		r.Error(c, e.ERROR_DATABASE, err.Error())
 		return
 	}
-  r.OK(c, e.SUCCESS, map[string]interface{}{"avatar_frame_list": avatar_frame_list, "total": len(avatar_frame_list)})
+	r.OK(c, e.SUCCESS, map[string]interface{}{"avatar_frame_list": avatar_frame_list, "total": len(avatar_frame_list)})
 }
 
 // @method [post]
@@ -34,10 +34,10 @@ func GetAllAvatarFrame(c *gin.Context) {
 func UploadAvatarFrame(c *gin.Context) {
 	addr := c.PostForm("addr")
 	comment := c.PostForm("comment")
-  t := c.PostForm("type") // Type of the Avatar Frame
-  n := c.PostForm("name") // Name of the Avatar Frame -> Unique
+	t := c.PostForm("type") // Type of the Avatar Frame
+	n := c.PostForm("name") // Name of the Avatar Frame -> Unique
 	Ret, err := models.AddNewAvatarFrame(addr, comment, t, n)
-	if err != nil{
+	if err != nil {
 		logging.Error("Upload Avatar Error: %v", err)
 		r.Error(c, e.ERROR_DATABASE, err.Error())
 		return
@@ -50,13 +50,13 @@ func UploadAvatarFrame(c *gin.Context) {
 // @param
 // @return
 // @route /b/frame/upload
-func UpdateAvatarFrame(c *gin.Context){
+func UpdateAvatarFrame(c *gin.Context) {
 	id := c.PostForm("id")
 	addr := c.PostForm("addr")
 	comment := c.PostForm("comment")
-  t := c.PostForm("type")
-  n := c.PostForm("name")
-  h := c.PostForm("hidden")
+	t := c.PostForm("type")
+	n := c.PostForm("name")
+	h := c.PostForm("hidden")
 	ret, err := models.UpdateAvatarFrame(util.AsUint(id), addr, comment, t, n, h)
 	if err != nil {
 		logging.Error("Update Avatar Frame Error: %v", err)
--- models/avatar_frame.go
+++ models/avatar_frame.go
@@ -8,27 +8,25 @@ import (
 )
 
 type AvatarFrame struct {
-	Id        	uint64 `gorm:"primaryKey;autoIncrement;" json:"id"`
-	Addr        string `json:"addr"`
-	CreatedAt 	string `json:"created_at" gorm:"default:null;"`
-	Comment     string `json:"comment"`
-  Type        string `json:"type"`
-  Name        string `json:"name"`
-  Hidden      string `json:"hidden"`
+	Id        uint64 `gorm:"primaryKey;autoIncrement;" json:"id"`
+	Addr      string `json:"addr"`
+	CreatedAt string `json:"created_at" gorm:"default:null;"`
+	Comment   string `json:"comment"`
+	Type      string `json:"type"`
+	Name      string `json:"name"`
+	Hidden    string `json:"hidden"`
 }
 
 // GetAllAvatarFrames 获取所有 AvatarFrame
-func GetAllAvatarFrames(f int) (avatar_frame_list []AvatarFrame, err error){
-  d := db
-  if f==1 {
-    // 1 是必须要 hidden 
-    d = d.Where("hidden = ?", false)
-  }
+func GetAllAvatarFrames(f int) (avatar_frame_list []AvatarFrame, err error) {
+	d := db
+	if f == 1 {
+		// 1 是必须要 hidden
+		d = d.Where("hidden = ?", false)
+	}
 	err = d.Order("id").Find(&avatar_frame_list).Error
-	return 
+	return
 }
-
-
 
 // GetAddrById 通过 id 获取整个 AvatarFrame
 func GetAddrById(id uint64) (avatar_frame AvatarFrame, err error) {
@@ -41,34 +39,34 @@ func GetAddrById(id uint64) (avatar_frame AvatarFrame, err error) {
 
 // AddNewAvatarFrame 添加一条数据
 func AddNewAvatarFrame(addr string, comment string, t string, n string) (avatar_frame AvatarFrame, err error) {
-  avatar_frame = AvatarFrame{Addr: addr, Comment: comment, Type: t, Name: n}
+	avatar_frame = AvatarFrame{Addr: addr, Comment: comment, Type: t, Name: n}
 	err = db.Select("addr", "comment", "type", "name").Create(&avatar_frame).Error
 	return
 }
 
 // UpdateAvatarFrame 更新存储地址
-func UpdateAvatarFrame(id uint64, addr string, comment string, t string, n string, h string) (avatar_frame AvatarFrame, err error){
+func UpdateAvatarFrame(id uint64, addr string, comment string, t string, n string, h string) (avatar_frame AvatarFrame, err error) {
 	err = db.First(&avatar_frame, id).Error
-	if errors.Is(err, gorm.ErrRecordNotFound){
+	if errors.Is(err, gorm.ErrRecordNotFound) {
 		return avatar_frame, err
 	}
 	avatar_frame.Comment = comment
 	avatar_frame.Addr = addr
-  avatar_frame.Type = t
-  avatar_frame.Name = n
-  avatar_frame.Hidden = h
+	avatar_frame.Type = t
+	avatar_frame.Name = n
+	avatar_frame.Hidden = h
 	err = db.Save(&avatar_frame).Error
-	return 
+	return
 }
 
 // GetAddrByType 通过 type 获取 avatar_frame_list
 func GetAddrByType(t string) (avatar_frame_list []AvatarFrame, err error) {
-  if err = db.Where("type = ? AND hidden = ?", t, false).Order("id").Find(&avatar_frame_list).Error; err != nil{
-    logging.Error("通过 type 获取 avatar_frame_list 错误")
-    if errors.Is(err, gorm.ErrRecordNotFound){
-      err = nil
-      return
-    }
-  }
-  return
+	if err = db.Where("type = ? AND hidden = ?", t, false).Order("id").Find(&avatar_frame_list).Error; err != nil {
+		logging.Error("通过 type 获取 avatar_frame_list 错误")
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			err = nil
+			return
+		}
+	}
+	return
 }
